fae542bb96f6a1e6df14e6b6625767c0dcca6fa8
Thiago Soares <tcsoares84@gmail.com>
2024-07-25 00:48:02 -0300

    feat: create and edit forms
    - create page for for new schedule;
    - create page for edit schedule;

--- src/app/schedules/create/page.tsx
+++ src/app/schedules/create/page.tsx
@@ -8,6 +8,108 @@ export const metadata: Metadata = {
 export default function CreateSchedule() {
   return (
     <>
+      <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <div className="flex flex-wrap mt-4">
+          <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+              <div className="rounded-t mb-0 px-4 py-3 border-0">
+                <div className="flex flex-wrap items-center">
+                  <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                    <h3 className="font-semibold text-base text-blueGray-700">
+                      Schedules
+                    </h3>
+                  </div>
+                  <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right"></div>
+                </div>
+              </div>
+              <div className="rounded-t mb-0 px-4 py-3 border-0">
+                <div>
+                  <p className="mt-1 text-sm leading-6 text-gray-600">* Required fields</p>
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="plate"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Plate
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          name="plate"
+                          id="plate"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="hour"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Type
+                      </label>
+                      <div className="mt-2">
+                        <select
+                          id="type"
+                          name="type"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6">
+                          <option>Choose type</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="day"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Day
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="date"
+                          name="day"
+                          id="day"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6" 
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="type"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Hour
+                      </label>
+                      <div className="mt-2">
+                        <select
+                          id="hour"
+                          name="hour"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6">
+                          <option>Choose hour</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="sm:col-span-3">
+                      <div className="mt-2">
+                        <button
+                          className="bg-green-900 text-white hover:bg-green-600 active:bg-green-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1" 
+                        >
+                          Create
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
